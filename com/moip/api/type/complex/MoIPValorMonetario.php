0e1cbcdebf3334fc39194aa103afd3b4dd590866
João Batista Neto <neto.joaobatista@gmail.com>
2011-08-11 14:18:55 -0300

    Adicionando propriedade valor

--- com/moip/api/type/complex/MoIPValorMonetario.php
+++ com/moip/api/type/complex/MoIPValorMonetario.php
@@ -6,6 +6,11 @@ final class MoIPValorMonetario {
 	private $moeda;
 
 	/**
+	 * @var	float
+	 */
+	private $valor;
+
+	/**
 	 * Recupera o valor de $moeda
 	 * @return	MoIPMoeda
 	 */
@@ -14,9 +19,24 @@ final class MoIPValorMonetario {
 	}
 
 	/**
+	 * Recupera o valor de $valor
+	 * @return	float
+	 */
+	public function getValor() {
+		return $this->valor;
+	}
+
+	/**
 	 * @param	MoIPCodigoMoeda $moeda
 	 */
 	public function setMoeda( MoIPCodigoMoeda $moeda ) {
 		$this->moeda = $moeda;
+	}
+
+	/**
+	 * @param	float $valor
+	 */
+	public function setValor( $valor ) {
+		$this->valor = (float) $valor;
 	}
 }
